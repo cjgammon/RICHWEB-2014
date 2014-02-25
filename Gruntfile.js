f0ae113d9ef366c317aeae7c6964c72a1d7138ee
cjgammon <cj.gammon@gmail.com>
2014-02-25 10:12:21 -0800

    include external demos in gruntjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
                         src: [
 							'assets/**', 
 							'templates/*',
-							'external/*'
+							'external/**'
 							],
                         dest: './site/'
                     }
